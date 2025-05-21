ee16f26853512a6421b6cabca793fae8284102fe
razanpixell <razanjun461@gmail.com>
2025-05-21 15:14:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,13 @@ st.write(
     "warzo_all_the_time."
 )
 st.image("Screenshot_20250423-175640~2.png")
+
+st.title("WARUNG SI ECEU")
+st.header("WARUNG ECEU SUKA RAME")
+angka = st.number_input("2:", value=0, step=1
+
+if (angka % 2) ==0   
+  st.write(f"{angka} itu adalah bilangan genap")
+ else:    
+  st.write(f"{angka} itu adalah bilangan ganjil")
+                        
